2a4bd85c2c4ab95824117dc441561fdf2363e701
Alexander <mrbeachcode@gmail.com>
2019-10-13 13:50:10 -0400

    commented the js file for readability

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
+//THIS ADDS AN EVENT LISTENER TO THE SUBMIT BUTTON TO CHANGE THE BRUSH COLOR ON CLICK
 document.addEventListener('click', changeBrush);
 
+//THIS FUNCTION CHANGES THE BRUSH COLOR TO THE VALID INPUT ON SUBMISSION
 function changeBrush(e) {
   e.preventDefault();
   const brushColor = document.querySelector('input').value;
   document.querySelector('.brush').style.background = brushColor;
 }
 
-
+//THIS CREATES THE PIXEL-CANVAS AT THE BOTTOM OF THE SCREEN
+//IT ALSO ADDS EVENT LISTENERS SO THAT THE WHEN THE CURSOR MOVES OVER OVER A SQUARE THE COLOR CHANGES
   for (let i = 0; i < 8000; i++) {
     var div = document.createElement('div');
     div.setAttribute('class', 'square');
@@ -15,6 +18,7 @@ function changeBrush(e) {
     document.body.appendChild(div);
 }
 
+//THIS IS THE FUNCTION THAT THE EVENT LISTENER USES TO CHANGE THE CANVAS COLOR ON MOUSE OVER
 function changeSquare(e){
   e.preventDefault();
   this.style.background = document.querySelector('input').value;
